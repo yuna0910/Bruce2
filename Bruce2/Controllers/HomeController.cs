b3333d84806fe40968168ee4f65f668b0748c00c
LAPTOP-RHT5F6T7\Yuna <yuna.zhang@collaborate.tw>
2024-06-09 11:51:50 +0800

    加入專案檔案。

---
+++ Bruce2/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bruce2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string answer, object feedback)
+        {
+            
+            if (answer == "0626")
+            {
+                feedback = "正确答案!";
+                feedback = "green";
+            }
+            else
+            {
+                feedback = "错误答案，请再试一次。";
+                feedback = "red";
+            }
+            return View();
+        }
+    }
+}
